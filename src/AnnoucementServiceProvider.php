6a5c6efafeaaf514fc3d4b663e3da910d2ba02fd
Emmanuel <epmnzava@gmail.com>
2021-06-10 13:29:41 +0300

    made fix on database migrations

--- src/AnnoucementServiceProvider.php
+++ src/AnnoucementServiceProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Author: Emmanuel Paul Mnzava
  * Twitter: @epmnzava
@@ -28,14 +29,16 @@ class AnnoucementServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('annoucement.php'),
+                __DIR__ . '/../config/config.php' => config_path('annoucement.php'),
             ], 'config');
 
 
             //publishing migrations here..
             if (!class_exists('CreateUserAnnoucementTable') && !class_exists('CreateAnnoucementTypeTable') && !class_exists('CreateAnnoucementTable')) {
                 $this->publishes([
+
                     __DIR__ . '/../database/migrations/create_annoucement_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_annoucement_table.php'),
+
                     __DIR__ . '/../database/migrations/create_annoucement_types_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_annoucement_types_table.php'),
                     __DIR__ . '/../database/migrations/create_user_annoucement_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_user_annoucement_table.php'),
 
@@ -70,7 +73,7 @@ class AnnoucementServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'annoucement');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'annoucement');
 
         // Register the main class to use with the facade
         $this->app->singleton('annoucement', function () {
